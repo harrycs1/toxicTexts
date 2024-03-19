ea4f7154d2962a75be03f735f5880c2d886a34e8
harrycs1 <98349746+harrycs1@users.noreply.github.com>
2024-03-19 16:08:07 +0000

    this commit configures the api to return combined toxicity score for all messages

--- api.js
+++ api.js
@@ -1,13 +1,34 @@
 // import * as tf from '@tensorflow/tfjs-node';
 import * as toxicity from '@tensorflow-models/toxicity';
+import * as fs from 'node:fs'
 
 // The minimum prediction confidence.
 const threshold = 0.9;
 
+let sentences = []
+let cleanedData = []
+
+try {
+  const data = fs.readFileSync('./data/dummyConversation.txt', 'utf8')
+
+  let lines = data.split('\n')
+  for (const line of lines) {
+    let date = line.split(',')[0].slice(1)
+    let time = line.slice(13).split(']')[0]
+    let name = line.slice(23).split(':')[0]
+    let message = line.slice(25 + name.length)
+
+    cleanedData.push([date, time, name, message])
+    sentences.push(message)
+  }
+} catch (error) {
+  console.log(error)
+}
+
 // Load the model. Users optionally pass in a threshold and an array of
 // labels to include.
 toxicity.load(threshold).then(model => {
-  const sentences = ['you suck', 'no you', 'why not'];
+  // const sentences = ['you suck', 'no you', 'why not', 'suck your mum'];
 
   model.classify(sentences).then(predictions => {
     // `predictions` is an array of objects, one for each prediction head,
@@ -15,7 +36,7 @@ toxicity.load(threshold).then(model => {
     // final prediction in `match` (either `false` or `true`).
     // If neither prediction exceeds the threshold, `match` is `null`.
 
-    console.log(predictions);
+    // console.log(predictions);
     /*
     prints:
     {
@@ -34,5 +55,18 @@ toxicity.load(threshold).then(model => {
     },
     ...
      */
+
+    let results = {}
+
+    predictions.forEach((prediction) => {
+      let label = prediction.label
+      let combinedProbability = prediction.results.map((result) => {
+        return result.probabilities[1]
+      }).reduce((a, b) => a + b) / prediction.results.length
+
+      results[label] = combinedProbability.toFixed(2)
+    })
+
+    console.log(results)
   });
 });
